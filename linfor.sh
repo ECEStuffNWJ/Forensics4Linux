41eb83c635e24d6f2ec26ca0462573a0bd4c336e
Nathan W Jones <ducatinat@gmail.com>
2024-05-08 10:19:16 +0800

    Update linfor.sh

--- linfor.sh
+++ linfor.sh
@@ -4,108 +4,133 @@
 script /tmp/F4L.txt
 
 # Last user login
+echo "Showing last logged in users..."
 $ lastlog
 $ last
 
 # Users with login shells
+echo "Showing laogin shellss..."
 $ cat /etc/passwd | grep sh$
 
 # List users’ cron
+echo "Showing cron users..."
 $ for user in $(cat /etc/passwd | cut -f1 -d: ); do echo $user; crontab -u $user -l; done
 
 # users with shells only
+echo "Showing shells only..."
 $ for user in $(cat /etc/passwd | grep sh$ | cut -f1 -d: ); do echo $user; crontab -u $user -l; done
 
 # SSH authorized keys
+echo "Showing SSH auth keys..."
 $ find / -type f -name authorized_keys
 
 # Show process tree with username, TTY, and wide output.
+echo "Showing TTY Process ..."
 $ ps auxfww
 
 # Process details
+echo "Showing process details..."
 $ lsof -p [pid]
 
 # Show all connections don’t resolve names (IP only)
+echo "Showing conections..."
 $ lsof -i -n
 $ netstat -anp
 
 # Look for tcp only
+echo "Showing tcp..."
 $ netstat -antp
 $ ss -antp
 
 # List all services
+echo "Showing lall services..."
 $ service --status-all
 
 # List firewall rules
+echo "Showing firewall rules..."
 $ iptables --list-rules
 
 # List all timers
+echo "Showing timers..."
 $ systemctl list-timers --all
 
 # Look to these file to see if the DNS has been poisoned.
-/etc/hosts
-/etc/resolv.conf
+echo "Showing DNS poisioning..."
+cat /etc/hosts
+cat /etc/resolv.conf
 
 # Files and Folders
 Show list files and folder with nano timestamp, sort by modification time (newest).
+echo "Showing nano tmestamps..."
 $ ls --full-time -lt
 
-# List all files that were modified on a specific date/time. List files which were modified on 2021-06-16 (YYYY-MM-DD)
-$ find / -newermt "2021-06-16" -ls 2>/dev/null
+# List all files that were modified on a specific date/time. 
+echo "Showing file edits..."
+$ find / -newermt "2021-06-24" -ls 2>/dev/null
 
-# List files which were modified on 2021-05-01 until 2021-05-09 (9 days ago)
-$ find / -newermt "2021-05-01" ! -newermt "2021-05-10" -ls 2>/dev/null
+# List files which were modified 9 days ago
+echo "Showing file edits 9 days ago..."
+$ find / -newermt "2021-05-24" ! -newermt "2021-05-24" -ls 2>/dev/null
 
-# List files which were modified on 2021-05-01 until 2021-05-09 (9 days ago) + add filter
-$ find / -newermt "2021-05-01" ! -newermt "2021-05-10" -ls 2>/dev/null | grep -v 'filterone\|filtertwo'
-
-# List files modified between 01:00 and 07:00 on June 16 2021.
-$ find / -newermt "2021-06-16 01:00:00" ! -newermt "2021-06-16 07:00:00" -ls 2>/dev/null
+# List files modified between 01:00 and 07:00 on June 16 2024.
+echo "Showing file edits..."
+$ find / -newermt "2024-06-16 01:00:00" ! -newermt "2024-06-16 07:00:00" -ls 2>/dev/null
 
 # List files that were accessed exactly 2 days ago.
+echo "Showing files accese last 2 days..."
 $ find / -atime 2 -ls 2>/dev/null
 
 # List files that were modified in the last 2 days.
+echo "Showing files mofified 2 days ago..."
 $ find / -mtime -2 -ls 2>/dev/null
 
+# EDIT FOR FILE INSPECTION
 # File inspection
-$ stat [file]
-$ exiftool [file]
+# echo "Showing file inspection..."
+# $ stat [file]
+# $ exiftool [file]
 
 # Observe changes in files
+echo "Showing file changes..."
 $ find . -type f -exec md5sum {} \; | awk '{print $1}' | sort | uniq -c | grep ' 1 ' | awk '{print $2	}'
 
 # Look for cap_setuid+ep in binary capabilities
+echo "Showing SET UID..."
 $ getcap -r /usr/bin/
 $ getcap -r /bin/
 $ getcap -r / 2>/dev/null
 
 # SUID
+echo "Showing SUId..."
 $ find / -type f -perm -u=s 2>/dev/null
 
-# Log auditing
-# 3rd party
+# Log auditing 3rd party
+echo "Showing auditing..."
 $ aureport --tty
 
-#Persistence areas Directories and Files
-/etc/cron*/
-/etc/incron.d/*
-/etc/init.d/*
-/etc/rc*.d/*
-/etc/systemd/system/*
-/etc/update.d/*
-/var/spool/cron/*
-/var/spool/incron/*
-/var/run/motd.d/*
+# EDIT FOR PERSISTENCE IN DIRECTORIES
+# Persistence areas Directories and Files
+# echo "Showing Persistence ..."
+# /etc/cron*/
+# /etc/incron.d/*
+# /etc/init.d/*
+# /etc/rc*.d/*
+# /etc/systemd/system/*
+# /etc/update.d/*
+# /var/spool/cron/*
+# /var/spool/incron/*
+# /var/run/motd.d/*
 #Files:
-/etc/passwd
-/etc/sudoers
-/home/<user>/.ssh/authorized_keys
-/home/<user>/.bashrc
+# /etc/passwd
+# /etc/sudoers
+# /home/<user>/.ssh/authorized_keys
+#/home/<user>/.bashrc
 #
 # https://sechive1.wixsite.com/security-hive/post/linux-forensics-the-complete-cheatsheet
+
 # PHASE 0 Risk Audit
 #Password hunting
+echo "Showing password dunters..."
 grep --color=auto -rnw '/' -ie "PASSWORD" --color=always 2> /dev/null
 find . -type f -exec grep -i -I "PASSWORD" {} /dev/null \;
 #search for possible Privilege Escalation Paths
@@ -119,6 +144,7 @@ wget "https://raw.githubusercontent.com/diego-treitos/linux-smart-enumeration/ma
 ./lse.sh -l2 # dump all the information it gathers about the system
 #
 PHASE 1 Users and Groups
+echo "Showing groups and users..."
 user List
 cat /etc/passwd
 # user creation
@@ -133,61 +159,79 @@ cat /etc/sudoers
 
 #PHASE 2 system Config
 # network
+echo "Showing system configs..."
 cat /etc/network/interfaces
 cat /etc/resolve.conf
 cat /etc/dnsmasq.conf
 # OS
+echo "Showing OS..."
 cat /etc/os-release
 # hostnAME
+echo "Showing hostname..."
 cat /etc/hostname
 # TIMEZONE
+echo "Showing timezone..."
 cat /etc/timezone
 
 # Phase 3 uSERS aCTIVITIES
 # RECENT ACCESS
+echo "Showing recent acces..."
 find . -type f -atime -7 -printf “%AY%Am%Ad%AH%AM%AS %h/%s/%f\n” -user |sort -n
 find . -type f -mtime -7 -printf “%TY%Tm%Td%TH%TM%TS %h — %s — %f\n” -user |sort -n
 find . -type f -ctime -7 -printf “%CY%Cm%Cd%CH%CM%CS %h — %s — %f\n” -user |sort -n
 # basH HISTORY
+echo "Showing history..."
 cat .bash_history
 cat .bashrc
-# mOUNT pOINTS
+# MOUNT POINTS
+echo "Showing mount points..."
 cat /proc/mounts
 
 # PHASE 4 Log Analysis
 # Log entries
+echo "Showing logs..."
 lastlog
 # Auth.Log
+echo "Showing auth logs..."
 cat /var/log/auth.log
 # Deamon.log
+echo "Showing deamon logs..."
 cat var/log/deamon.log
 # SysLog
+echo "Showing syslog..."
 cat var/log/syslog
 # WTMP
+echo "Showing WTMP..."
 cat /var/log/btmp
-# Appication Logs ????
- Some of those logs to name are apache2, httpd, samba, MySQL etc.
 
 #PHASE 5 Persistance
 # Services
+echo "Showing services..."
 services –status-all
 # Processes
+echo "Showing processes..."
 top
 ps aux
 lsof -p [pid]
 # Scheduled Jobs
+echo "Showing scheduled jobs..."
 cat /etc/crontab
 # DNS Resolves
+echo "Showing last DNS resolves..."
 cat /etc/resolve.conf
 # Firewall Rules
+echo "Showing firewall rules..."
 cat /etc/resolve.conf
 # Network Connections
+echo "Showing network connections..."
 netstat -nap
 
 # IMAGE TOOLS
+echo "Showing mem image..."
 fmem
 
-# Memoery
+# Memory
+echo "Showing memory profile..."
 Lime (http://code.google.com/p/lime-forensics/)
 adb push lime.ko /sdcard/lime.ko
 adb forward tcp:4444 tcp:4444
@@ -196,7 +240,7 @@ su
 insmod /sdcard/lime.ko "path=tcp:4444 format=lime"
 exit
 
-3 second run
+# second run with different approach
 # Redirecting output to Forensics4Linux.txt in /tmp/ directory
 exec > /tmp/Forensics4Linux.txt 2>&1
 
